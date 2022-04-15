51e84b5950b2a04ff321160f2c0e666383af8873
ThomasDCW <thomas.dicola@hotmail.fr>
2022-04-15 22:13:06 +0200

    add images to HomePageHeader

--- frontend/src/components/HomePage/HomePageHeader/index.jsx
+++ frontend/src/components/HomePage/HomePageHeader/index.jsx
@@ -1,8 +1,18 @@
 import SHomePageHeader from "./style";
+import marvel from "../..//../assets/headerhomepage/marvel.jpg";
+import starwars from "../..//../assets/headerhomepage/starwars.jpg";
+import manga from "../..//../assets/headerhomepage/Manga.jpg";
+import dccomics from "../..//../assets/headerhomepage/dccomics.jpg";
+import autres from "../..//../assets/headerhomepage/autres.jpg";
+
 export default function HomePageHeader() {
   return (
     <SHomePageHeader>
-      <h1>Hello</h1>
+      <img src={marvel}></img>
+      <img src={starwars}></img>
+      <img src={manga}></img>
+      <img src={dccomics}></img>
+      <img src={autres}></img>
     </SHomePageHeader>
   );
 }
--- frontend/src/components/HomePage/HomePageHeader/style.jsx
+++ frontend/src/components/HomePage/HomePageHeader/style.jsx
@@ -1,5 +1,15 @@
 import styled from "styled-components";
 
 export default styled.header`
-  background: tomato;
+  display: flex;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+  img {
+    margin-top: 2vh;
+    margin-right: 2vw;
+    width: 17%;
+    height: 60vh;
+    object-fit: cover;
+  }
 `;
