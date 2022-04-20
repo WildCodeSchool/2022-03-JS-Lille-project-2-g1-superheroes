da97e4207823e7fd6432c9a8a6dfd8d8cc8b3a98
Laura Courquin <lauracourquin@outlook.com>
2022-04-20 10:06:21 +0200

    Rename fil card.jsx to index.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import { Routes, Route } from "react-router-dom";
-import Card from "./components/Cards/Card";
+import Card from "./components/Cards";
 import "./App.css";
 import Carousel from "./components/Carousel";
 import Footer from "./components/Footer";
 import LegalNotice from "./components/LegalNotice";
-import Contact from "./components/Contact/Contact";
+import Contact from "./components/Contact";
 
 function App() {
   return (
