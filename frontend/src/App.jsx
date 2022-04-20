e8ce3dc0e2c2d5dd6f4698ea6bac059729ceb686
Laura Courquin <lauracourquin@outlook.com>
2022-04-20 09:46:38 +0200

    Rename files contact.jsx, legalNotice.jsx and footer.jsx to index.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,14 @@
-import Card from "@components/Cards/Card";
-// import Home from "@pages/Home";
-
 import { Routes, Route } from "react-router-dom";
+import Card from "./components/Cards/Card";
 import "./App.css";
 import Carousel from "./components/Carousel";
-import Footer from "./components/Footer/Footer";
-import LegalNotice from "./components/LegalNotice/LegalNotice";
+import Footer from "./components/Footer";
+import LegalNotice from "./components/LegalNotice";
 import Contact from "./components/Contact/Contact";
 
 function App() {
   return (
     <div className="App">
-      {/* <Home /> */}
       <Card />
 
       <Routes>
