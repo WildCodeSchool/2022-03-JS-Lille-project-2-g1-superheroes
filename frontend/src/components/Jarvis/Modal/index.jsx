79bec0485fe735373048871722202316b2b60f9a
romainLF <romainlefloch@hotmail.com>
2022-04-21 21:43:39 +0200

    jarvis V1

--- frontend/src/components/Jarvis/Modal/index.jsx
+++ frontend/src/components/Jarvis/Modal/index.jsx
@@ -1,53 +1,52 @@
-import Jarvis from '@components/Jarvis'
-import {useState} from 'react';
+import { useState } from "react";
+import PropTypes from "prop-types";
+
 const MODAL_STYLES = {
-    position: 'fixed',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    backgroundColor: '#000000',
-    width: '45vw',
-    height: '50vh',
-    zIndex: 1000
-}
+  position: "fixed",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  backgroundColor: "#918989",
+  width: "45vw",
+  height: "50vh",
+  zIndex: 1000,
+};
 const OVERLAY_STYLES = {
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    backgroundColor: 'rgba(0, 0, 0, .7)',
-    zIndex: 1000
+  position: "fixed",
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  backgroundColor: "rgba(0, 0, 0, .7)",
+  zIndex: 1000,
+};
+
+function Modal({ open, onClose, children }) {
+  if (!open) {
+    return null;
+  }
+
+  const [titles, setTitles] = useState([]);
+
+  return (
+    <>
+      <div style={OVERLAY_STYLES} />
+      <div style={MODAL_STYLES}>
+        <button onClick={onClose} type="button">
+          Close Modal
+        </button>
+        {children}
+        <div className="app">
+          {titles.map((title) => {})}
+        </div>
+      </div>
+    </>
+  );
 }
-
-
-
-
-
-
-
-
-
-
-function Modal({open, children, onClose}) {
-    if (!open) return null
-
-    const [title, setTitle] = useState([]);
-
-    return (
-        <>
-            <div style={OVERLAY_STYLES} />
-            <div style={MODAL_STYLES}>
-                <button onClick ={onClose}>Close Modal</button>
-                {children}
-                <div className='app'>
-                    {title.map((title)=> {
-                        return ;
-                    })}
-                </div>
-            </div>
-        </>
-    )
-}
+Modal.propTypes = {
+  open: PropTypes.string.isBoolean,
+  onClose: PropTypes.string.isFunction,
+  children: PropTypes.string.isString
+};
 
 export default Modal;
--- frontend/src/components/Jarvis/index.jsx
+++ frontend/src/components/Jarvis/index.jsx
@@ -2,44 +2,46 @@ import imgJarvis1 from "@assets/jarvis-rond.png";
 import imgJarvis2 from "@assets/jarvis-centre.png";
 import imgJarvis3 from "@assets/jarvis-jarvis.png";
 import imgJarvis4 from "@assets/jarvisFD.jpeg";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import SJarvis from "./style";
 import Modal from "./Modal";
-import axios from "axios";
 
 export default function Jarvis() {
-    const [active, setActive] = useState("jarvisNav");
-    const [popCard, setPopCard] = useState({});
-    const toggleJarvis = () => {
+  const [active, setActive] = useState("jarvisNav");
+  const [popCard, setPopCard] = useState({});
+  const [filterValue, setFilterValue] = useState("")
+
+  const toggleJarvis = () => {
     if (active === "jarvisNav") {
       setActive("jarvisNav jarvisActive ");
     } else setActive("jarvisNav");
   };
-
   const [isOpen, setIsOpen] = useState(false);
 
-  //--------API--------//
-  const getQuoteCard = () => {
-    axios
-    .get("http://localhost:5000/heroes")
-    .then(({ data }) => {setPopCard(data[0]);
+  const search = () => {
+    axios.get(`http://localhost:5000/heroes`)
+    .then(({data}) => {
+      // insert your filter here <3
+        const myHeros = data.filter((hero) => {
+          return hero.name.includes(filterValue);
+      });
+      setPopCard(myHeros[0]);
     });
-  };
-
-  const getFinalCard = (e) => {
-      getQuoteCard();
   }
 
-
   return (
-    <SJarvis>
-        {<Modal open={isOpen} onClose={()=> setIsOpen(false)}>
-            {popCard.name}
-        </Modal>}
+    <SJarvis> 
+      {popCard?.name && <Modal open={isOpen} onClose={() => setIsOpen(false)}>
+    {popCard?.name}
+  </Modal>}
       <div className="block">
         <img src={imgJarvis1} alt="jarvis1" className="img1" />
-        <button type="button" onMouseOver={()=> toggleJarvis(false)}
-                            onClick={()=> setIsOpen(true)}>
+        <button 
+          type="button"
+          onMouseOver={() => toggleJarvis(false)}
+          onClick={() => {setIsOpen(true); search()}}
+        >
           <img
             src={imgJarvis2}
             alt="toggleJarvis"
@@ -54,8 +56,9 @@ export default function Jarvis() {
           <img src={imgJarvis3} alt="jarvis3" className="img3" />
           <input
             name="myinput"
-            type="text"
-            onBlur={(e) => getFinalCard(e.target.value)}
+            value={filterValue}
+            type="search"
+            onChange={(e) => {setFilterValue(e.target.value)}}
             placeholder="Votre recherche"
           />
         </div>
