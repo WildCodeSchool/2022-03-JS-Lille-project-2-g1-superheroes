7b19a00c7e72503dbe998fa53d61612730193b6d
Jeremy <jeremy.jorand@gmail.com>
2022-04-21 11:21:27 +0200

    fix link and style homePage

--- frontend/src/components/HomePage/HomePageHeader/index.jsx
+++ frontend/src/components/HomePage/HomePageHeader/index.jsx
@@ -1,31 +1,29 @@
 import { Link } from "react-router-dom";
 import SHomePageHeader from "./style";
-import marvel from "../../../assets/headerhomepage/marvel.jpg";
-import starwars from "../../../assets/headerhomepage/starwars.jpg";
-import manga from "../../../assets/headerhomepage/Manga.jpg";
-import dccomics from "../../../assets/headerhomepage/dccomics.jpg";
-import autres from "../../../assets/headerhomepage/autres.jpg";
 
 export default function HomePageHeader() {
   return (
     <SHomePageHeader>
-      <section className="link">
-        <Link to="/marvel">
-          <img src={marvel} alt="marvel" />
-        </Link>
-        <Link to="/starwars">
-          <img src={starwars} alt="starwars" />
-        </Link>
-        <Link to="/manga">
-          <img src={manga} alt="manga " />
-        </Link>
-        <Link to="/dccomics">
-          <img src={dccomics} alt="dccomics" />
-        </Link>
-        <Link to="/autresheros">
-          <img src={autres} alt="autresheros" />
-        </Link>
-      </section>
+      <nav className="imgLink">
+        <ul>
+          <Link to="/marvel">
+            <li className="marvel"> </li>
+          </Link>
+          <Link to="/starwars">
+            <li className="starwars"> </li>
+          </Link>
+          <Link to="/manga">
+            <li className="manga"> </li>
+          </Link>
+          <Link to="/dccomics">
+            <li className="dccomics"> </li>
+          </Link>
+          <Link to="/autresheros">
+            <li className="autresheros"> </li>
+          </Link>
+        </ul>
+        {/*  */}
+      </nav>
     </SHomePageHeader>
   );
 }
--- frontend/src/components/HomePage/HomePageHeader/style.jsx
+++ frontend/src/components/HomePage/HomePageHeader/style.jsx
@@ -1,18 +1,44 @@
 import styled from "styled-components";
+import marvel from "../../../assets/headerhomepage/marvel2.jpg";
+import starwars from "../../../assets/headerhomepage/starwars2.jpg";
+import manga from "../../../assets/headerhomepage/Manga.jpg";
+import dccomics from "../../../assets/headerhomepage/dccomics.jpg";
+import autres from "../../../assets/headerhomepage/autres.jpg";
 
-export default styled.header`
-  margin-top: 2vh;
-  display: flex;
-  justify-content: space-evenly;
-  width: 100vw;
-  height: 60vh;
-  a img {
-    width: 17%;
-    object-fit: cover;
-    filter: grayscale(80%);
+export default styled.nav`
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    display: flex;
+    justify-content: space-evenly;
+    width: 100vw;
+    li {
+      background-size: cover;
+      background-repeat: no-repeat;
+      background-position: center;
+      border: solid 2px black;
+      width: 18vw;
+      height: 70vh;
+      filter: grayscale(80%);
+    }
+    li:hover {
+      filter: grayscale(0);
+    }
   }
-  a img:hover {
-    cursor: pointer;
-    filter: none;
+  .marvel {
+    background-image: url(${marvel});
+  }
+  .starwars {
+    background-image: url(${starwars});
+  }
+  .manga {
+    background-image: url(${manga});
+  }
+  .dccomics {
+    background-image: url(${dccomics});
+  }
+  .autresheros {
+    background-image: url(${autres});
   }
 `;
