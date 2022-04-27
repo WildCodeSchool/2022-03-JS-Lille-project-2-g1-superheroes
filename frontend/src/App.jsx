d33e928a10e71d02b4022c09db0c95e2bcc92fbc
HAMZA BENIDAR <93709799+Hamza-Luciano@users.noreply.github.com>
2022-04-27 14:59:57 +0200

    Update App.jsx
    add modification

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,17 @@
-import Card from "@components/Cards/Card";
-import Logo from "@components/Logo";
-import HomePage from "@components/HomePage";
 import { Routes, Route } from "react-router-dom";
-import HomePage from "./components/HomePage";
 import SGlobal from "./style";
 import Footer from "./components/Footer/Footer";
 import LegalNotice from "./components/LegalNotice/LegalNotice";
-import Contact from "./components/Contact/Contact";
-import Marvel from "./components/Marvel";
-import Starwars from "./components/Starwars";
-import Manga from "./components/Manga";
-import Dccomics from "./components/Dccomics";
-import Autresheros from "./components/Autresheros";
+
 
 function App() {
   return (
-    <div className="App">
-      <Card />
-      <Logo />
-      <HomePage />
+   
     <SGlobal className="App">
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/marvel" element={<Marvel />} />
-        <Route path="/starwars" element={<Starwars />} />
-        <Route path="/manga" element={<Manga />} />
-        <Route path="/dccomics" element={<Dccomics />} />
-        <Route path="/autresheros" element={<Autresheros />} />
-        <Route path="/legalnotice" element={<LegalNotice />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+            <Route path="/legalnotice" element={<LegalNotice />} />
+             </Routes>
       <Footer />
     </SGlobal>
   );
