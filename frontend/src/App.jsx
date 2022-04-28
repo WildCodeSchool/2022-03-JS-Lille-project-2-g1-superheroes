407fd1a1069c5cb2b2194154800282c9fbf08497
LoicBrassart <LoicBrassart@users.noreply.github.com>
2022-04-28 16:35:43 +0200

    Revert "create component Logo"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,27 @@
 import { Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage";
 import SGlobal from "./style";
 import Footer from "./components/Footer/Footer";
 import LegalNotice from "./components/LegalNotice/LegalNotice";
+import Contact from "./components/Contact/Contact";
+import Marvel from "./components/Marvel";
+import Starwars from "./components/Starwars";
+import Manga from "./components/Manga";
+import Dccomics from "./components/Dccomics";
+import Autresheros from "./components/Autresheros";
 
 function App() {
   return (
-   
     <SGlobal className="App">
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/marvel" element={<Marvel />} />
+        <Route path="/starwars" element={<Starwars />} />
+        <Route path="/manga" element={<Manga />} />
+        <Route path="/dccomics" element={<Dccomics />} />
+        <Route path="/autresheros" element={<Autresheros />} />
         <Route path="/legalnotice" element={<LegalNotice />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
     </SGlobal>
